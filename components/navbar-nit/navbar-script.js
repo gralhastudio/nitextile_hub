15c16723a5da556b1f61f5a139f91dacc86f1d09
Jose Almeida <gralhastudio@gmail.com>
2024-09-02 23:36:36 +0100

    update navbar nitextile

--- components/navbar-nit/navbar-script.js
+++ components/navbar-nit/navbar-script.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     const checkbox = document.querySelector('.mobile-menu .checkbox');
     const lines = document.querySelectorAll('.mobile-menu .hamburger-lines .line');
+    const mobileMenu = document.querySelector ('.mobile-menu-container .nav-links');
 
     checkbox.addEventListener('change', function () {
+        mobileMenu.classList.toggle('active');
         lines.forEach(line => line.classList.toggle('active'));
+        
     });
 });
